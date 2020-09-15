6b70d2e4682c5a37405f1c132064b4bd66064b7e
TheVinh <binkool281132442@gmail.com>
2020-09-16 00:25:58 +0700

    Updates

---
+++ precache-manifest.1052292cc3dae1c8fbb2f93549148691.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ae5d628d9e8f2adb53e225213f53ec",
+    "url": "/TodoWork/index.html"
+  },
+  {
+    "revision": "55662a12fc403298090c",
+    "url": "/TodoWork/static/css/main.b268ccff.chunk.css"
+  },
+  {
+    "revision": "9b7b1f4c8ecc2b1b2710",
+    "url": "/TodoWork/static/js/2.dfafacad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoWork/static/js/2.dfafacad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55662a12fc403298090c",
+    "url": "/TodoWork/static/js/main.d0a69bdb.chunk.js"
+  },
+  {
+    "revision": "6428ca1968ee47a06c2a",
+    "url": "/TodoWork/static/js/runtime-main.1d586108.js"
+  }
+]);
